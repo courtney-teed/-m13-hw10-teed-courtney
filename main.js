9ee715b6e8ef25f4d250510718d7cb32c7aad586
Courtney Teed <courtneyteed@Courtneys-MBP.attlocal.net>
2018-11-15 21:23:53 -0800

    Fix slideToggle function

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 //JavaScript Functionality Part 1 Goes Here. Comment it all out once finished.
 
-var section = document.getElementByTagName('section');
-var button = document.getElementByTagName('button');
+var section = document.getElementsByTagName('section');
+var button = document.getElementsByTagName('button');
 
 function slideToggle() {
-	section.classList.toggle('hide');
+	this.classList.toggle('hide');
 };
 
-button[i].addEventListener('click', slideToggle);
+for (let i = 0; i < button.length; i++) {
+	button[0].addEventListener('click', slideToggle);
+};
 
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
